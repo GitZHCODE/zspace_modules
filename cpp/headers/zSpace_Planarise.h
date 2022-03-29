693ee2c0ff9e8a7a49b32ba9e7edaa9c2ff0b9b8
Vishu Bhooshan <vishu.bhooshan@outlook.com>
2022-03-29 13:24:51 +0100

    updated planarise methods.

--- cpp/headers/zSpace_Planarise.h
+++ cpp/headers/zSpace_Planarise.h
@@ -41,26 +41,29 @@ namespace  zSpace
 	/*!	\brief container of particle function set  */
 	vector<zFnParticle> fnParticles;
 
+
 	zPointArray vertexPositions;
 
 	zInt2DArray polygons;
 
+	zInt2DArray triangles;
+
 	//--------------------------
 	//---- METHOD
 	//--------------------------
-	
-	ZSPACE_MODULES double quadPlanarityDeviation(int index, zPointArray &_vertexPositions, zInt2DArray& _polygons, zUtilsCore& _core);
-	
-	extern "C" ZSPACE_MODULES void quadPlanarise(double* _vertexPositions, int* _polyCounts, int* _polyConnects, int numVerts, int numFaces, bool updatetopology, int numIterations, double tolerance, double* outVertexPositions,  double* outDeviations);
+
+	ZSPACE_MODULES double quadPlanarityDeviation(int index, zPointArray& _vertexPositions, zInt2DArray& _polygons, zUtilsCore& _core);
+
+	ZSPACE_MODULES void computeFaceVolumes(zPointArray& _vertexPositions, zInt2DArray& _polygons, zInt2DArray& _triangles, zUtilsCore& _core, zPointArray& fCenters, zFloatArray& fVolumes);
+
+	ZSPACE_MODULES void computeFaceNormals(zPointArray& _vertexPositions, zInt2DArray& _polygons, zInt2DArray& _triangles, zUtilsCore& _core, zVectorArray& fNormals);
+
+	extern "C" ZSPACE_MODULES void quadPlanarise(double* _vertexPositions, int* _polyCounts, int* _polyConnects, int numVerts, int numFaces, bool updatetopology, int numIterations, double tolerance, double* outVertexPositions, double* outDeviations);
 
 	extern "C" ZSPACE_MODULES void planarise(double* _vertexPositions, int* _polyCounts, int* _polyConnects, int* _triCounts, int* _triConnects, int numVerts, int numFaces, bool updatetopology, int numIterations, double tolerance, double* outVertexPositions, double* outDeviations);
 
+
 }
 
-#if defined(ZSPACE_STATIC_LIBRARY)  || defined(ZSPACE_DYNAMIC_LIBRARY)
-// All defined OK so do nothing
-#else
-//#include<zSpace_Planarise.cpp>
-#endif
 
 #endif
